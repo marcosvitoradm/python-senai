4977bcb8edb999c6effd69852a8e0b9b99599dff
Marcos Vitor <marcosvitor.adm@gmail.com>
2024-08-28 07:28:22 -0300

    atualizado

--- aula-04/atividades/atividade-49.py
+++ aula-04/atividades/atividade-49.py
@@ -3,12 +3,13 @@ lista_01 = 0
 
 for i in range(7):
     num_01 = int(input(f'Digite aqui seu {i+1}º número: \n'))
-    lista_01.append(num_01)
+
 
 # Exiba quantos desses números são maiores que 10.
-if lista_01 > 10:
-        lista_01 +=1
-print(f' maiores que 10 {lista_01}')
+if num_01 > 10:
+    lista_01 += 1
+print(f' maiores que 10: {num_01}')
+
 
 
 
---
+++ aula-04/atividades/atividade-50.py
@@ -0,0 +1,15 @@
+# Peça ao usuário para inserir um número.
+numero = int(input('Insira um número: \n'))
+
+# Exiba os números de 1 até esse número, mas de forma decrescente.
+
+def mais1(numero):
+    contagem = -1
+    numero + contagem
+    contagem += 1
+    return contagem
+    
+for i in range(numero):
+    print(mais1)
+
+    
