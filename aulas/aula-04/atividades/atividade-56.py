1a4a3a108ee8dfe2f7a6ab166eb48e2e3253d472
Marcos Vitor <marcosvitor.adm@gmail.com>
2024-08-30 19:13:19 -0300

    mensagem do commit

--- aulas/aula-04/atividades/atividade-56.py
+++ aulas/aula-04/atividades/atividade-56.py
@@ -1,7 +1,14 @@
 # pergunte ao usuário quantas vezes ele quer que uma mensagem seja exibida. 
 
-
+aparicoes = int(input('Quantas vezes ele quer que uma mensagem seja exibida: \n'))
 
 
 # utilize um laço while para exibir a mensagem a quantidade de vezes desejada.
 
+mensagem = 'Bom dia!'
+
+contador = 0
+
+while contador < aparicoes
+    print(mensagem)
+    contador += 1
